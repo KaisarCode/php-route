d0f456a8e75a0dd8cb91f05d0a97423cdd76945e
KaisarCode <kaisar@kaisarcode.com>
2021-01-03 01:21:00 -0300

    Add functions file

--- route.php
+++ route.php
@@ -1,19 +1,15 @@
 <?php
-namespace KC;
-
 // Router
-Class Route {
-    public function __construct($mth, $pth, $cb) {
-        $mthd = $_SERVER['REQUEST_METHOD'];
-        $mthd = strtoupper($mthd);
-        $mthd = trim($mthd);
-        if ($mth == $mthd || $mth == 'all') {
-            $path = $_SERVER['REQUEST_URI'];
-            $path = parse_url($path, PHP_URL_PATH);
-            $path = '/'.trim($path, '/');
-            $patt = str_replace('/', '\\/', $pth);
-            preg_match_all("/^$patt$/", $path, $mtch)
-            && $cb($mtch);
-        }
+function route($mth, $pth, $cb) {
+    $mthd = $_SERVER['REQUEST_METHOD'];
+    $mthd = strtoupper($mthd);
+    $mthd = trim($mthd);
+    if ($mth == $mthd || $mth == 'all') {
+        $path = $_SERVER['REQUEST_URI'];
+        $path = parse_url($path, PHP_URL_PATH);
+        $path = '/'.trim($path, '/');
+        $patt = str_replace('/', '\\/', $pth);
+        preg_match_all("/^$patt$/", $path, $mtch)
+        && $cb($mtch);
     }
 }
