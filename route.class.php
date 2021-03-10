bbe23f082b75d76c97e8b50e699ed72ac5d23681
KaisarCode <kaisar@kaisarcode.com>
2021-03-10 03:22:08 -0300

    remove problematic trim

--- route.class.php
+++ route.class.php
@@ -10,7 +10,6 @@ Class Route {
         if ($mth == $mthd || $mth == 'ALL') {
             $path = $_SERVER['REQUEST_URI'];
             $path = parse_url($path, PHP_URL_PATH);
-            $path = '/'.trim($path, '/');
             $patt = str_replace('/', '\\/', $pth);
             preg_match_all("/^$patt$/", $path, $mtch)
             && $cb($mtch);
--- route.php
+++ route.php
@@ -7,7 +7,6 @@ function route($mth, $pth, $cb) {
     if ($mth == $mthd || $mth == 'ALL') {
         $path = $_SERVER['REQUEST_URI'];
         $path = parse_url($path, PHP_URL_PATH);
-        $path = '/'.trim($path, '/');
         $patt = str_replace('/', '\\/', $pth);
         preg_match_all("/^$patt$/", $path, $mtch)
         && $cb($mtch);
